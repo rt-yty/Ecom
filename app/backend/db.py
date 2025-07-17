9538f80ddd93f591940a9e61506fca54fc6f3b33
Aleksey_Emanov <aemanov878@gmail.com>
2025-07-17 16:06:12 +0300

    add docker

--- app/backend/db.py
+++ app/backend/db.py
@@ -8,7 +8,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=False)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 class Base(DeclarativeBase):
--- app/migrations/env.py
+++ app/migrations/env.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import pool
@@ -9,7 +10,16 @@ from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
+
+from dotenv import load_dotenv
+load_dotenv(".env")
+
 config = context.config
+
+database_url = os.getenv('DATABASE_URL')
+if not database_url:
+    raise ValueError('DATABASE_URL is not set')
+config.set_main_option('sqlalchemy.url', database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
